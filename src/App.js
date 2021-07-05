5b2e448019be10920f2cc6386fab239a5537b2dc
SIMONMHINI <mhinisimon99@gmail.com>
2021-07-05 12:29:50 +0300

    dsbdnsdkb

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import {BrowserRouter as Router, Switch,Route} from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <><Router>
+    <>
+        <Router>
       <Navbar/>
       <Switch>
         <Route path='/' exact/>
       </Switch>
     </Router>
-
-
-</>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,37 @@
-import React, {useState}  from "react";
+import React, {useState,useEffect}  from "react";
 import { Link } from "react-router-dom";
+import './Navbar.css';
+import { Button } from './Button';
   function Navbar(){
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+
     const handleClick = () =>  setClick(!click);
     const closeMobileMenu=() =>setClick(false);
 
+    const showButton =() =>{
+        if (window.innerWidth<= 960) {
+            setButton(false)
+        }else {
+            setButton(true);
+        }
+    };
+    useEffect(() =>{
+        showButton();
+    },[]);
+ window.addEventListener('resize',showButton);
+
     return(
         <>
-            <nav className="navbar">
+            <nav className="navbar" >
                 <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
-                        GRACIOUS < i className="fab fa-typo3"/>
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                        GRACIOUS
+                        < i className="fab fa-typo3"/>
                     </Link>
                     <div className="menu-icon" onClick={handleClick}>
-                        <i className={click ? "fas fa-times" : "fas far-bars"}/>
+                        <i className={click ? "fas fa-times" : "fas fa-bars"}/>
                     </div>
                     <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                         <li className='nav-item'>
@@ -21,8 +39,24 @@ import { Link } from "react-router-dom";
                         Home
                         </Link>
                         </li>
+                        <li className='nav-item'>
+                            <Link to='/service' className="nav-links" onClick={closeMobileMenu}>
+                                Service
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/product' className="nav-links" onClick={closeMobileMenu}>
+                                Product
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/sign-up' className="nav-links-mobile" onClick={closeMobileMenu}>
+                               Sign-up
+                            </Link>
+                        </li>
                     </ul>
-
+                    { button &&<Button buttonStyle='btn--outline'>SIGN UP
+                    </Button>}
                 </div>
             </nav>
         </>
